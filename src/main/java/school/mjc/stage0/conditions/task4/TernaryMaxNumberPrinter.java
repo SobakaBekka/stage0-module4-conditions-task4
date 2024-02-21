0cf9e16d5d04f9407128c79ff89b564b93b2e2eb
SobakaBekka <st69704@upce.cz>
2024-02-21 17:17:26 +0100

    add the brackets

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -4,7 +4,8 @@ package school.mjc.stage0.conditions.task4;
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
 
-        double max = first >= second ? (first >= third ? first : third) : second >= third ? second : third;
+        double max = first >= second ? (first >= third ? first : third) : (second >= third ? second : third);
+        //скобки для указания порядка действий
         System.out.println(max);
 
     }
