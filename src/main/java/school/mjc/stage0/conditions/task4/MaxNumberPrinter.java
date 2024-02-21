c79a04f58dade4f18151d314b2bd0bde9cb8ed8d
SobakaBekka <st69704@upce.cz>
2024-02-21 17:13:10 +0100

    done the tasks

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,23 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+
+
+        if (first > second && first > third) {
+            System.out.println(first);
+        } else if (second > first && second > third) {
+            System.out.println(third);
+        } else if (third > first && third > second) {
+            System.out.println(third);
+        } else {
+            if (first > second && first == third || first > third && third == second) {
+                System.out.println(first);
+            } else if (second > first && second == third || second > third && second == first) {
+                System.out.println(second);
+            } else if (third > first && third == second || third > second && third == first) {
+                System.out.println(third);
+            }
+        }
+
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -1,6 +1,13 @@
 package school.mjc.stage0.conditions.task4;
 
+
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+
+        double max = first >= second ? (first >= third ? first : third) : second >= third ? second : third;
+        System.out.println(max);
+
     }
-}
+        }
+
+
