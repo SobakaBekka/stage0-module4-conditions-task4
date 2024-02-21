8d3951e9d8aea76bef1237d4eeaeb43e0e2400b5
SobakaBekka <st69704@upce.cz>
2024-02-21 17:19:03 +0100

    fixed the bugs

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.task4;
 public class TernaryAssignment {
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
 
-        double result = first > second ? 10 : -10;
+        int result = first > second ? 10 : -10;
         System.out.println(result);
 
     }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -4,7 +4,7 @@ package school.mjc.stage0.conditions.task4;
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
 
-        double max = first >= second ? (first >= third ? first : third) : (second >= third ? second : third);
+        int max = first >= second ? (first >= third ? first : third) : (second >= third ? second : third);
         //скобки для указания порядка действий
         System.out.println(max);
 
