2dbb4343ebe6cbc5e755a8cdce28f2c50c4bda44
SobakaBekka <st69704@upce.cz>
2024-02-21 17:30:14 +0100

    return

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.task4;
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
 
-        System.out.println(first > second ? first : second);
+        System.out.println(first > second ? "first" : "second");
 
 
     }
